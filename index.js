16e0e1986685d67f00ded6060c74195a3af94ea3
gitluky <jluk87@gmail.com>
2019-10-21 21:50:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' w]+/g, '');
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of','and', 'for', 'at', 'by', 'from'];
+    const newStringArr = [];
+
+    const words = string.split(" ")
+    words.forEach((word) => {
+      exceptions.includes(word) ? newStringArr.push(word) : newStringArr.push(Formatter.capitalize(word));
+    });
+    newStringArr[0] = Formatter.capitalize(newStringArr[0]);
+    return newStringArr.join(" ");
+  }
 }
