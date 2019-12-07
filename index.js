112c258748990cd91d300e2a16f8ffddde2ae1df
Richard Cho <richardcho100@gmail.com>
2019-12-07 00:11:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string) {
+    let excludingWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let arr = string.split(" ")
+    const newArr = arr.map((element) => {
+      if (excludingWords.includes(element)) {
+        return element;
+      } else {
+        return this.capitalize(element);
+      }
+    })
+    return this.capitalize(newArr.join(" "));
+  }
 }
