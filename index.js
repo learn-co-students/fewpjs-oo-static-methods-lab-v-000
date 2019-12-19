b4c78649fd5253dc71c0baf7cc83af9e0bd9aa9a
Thomas Gray <thomas.gray@tgray.local>
2019-12-19 10:13:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-za-z0-9-'\s]/g, '').replace(/\^/g, '')
+  }
+
+  static titleize(str) {
+    const whitelist = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let arr = str.split(' ')
+    let newArr = []
+    newArr.push(this.capitalize(arr[0]))
+
+    arr.slice(1).map(word => whitelist.includes(word) ? newArr.push(word) : newArr.push(Formatter.capitalize(word)))
+
+    return newArr.join(' ')
+  }
 }
