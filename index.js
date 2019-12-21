c796689e67e3fe3332da4cb3886165bc598c2640
Rebecca Tong <rtong@buffalo.edu>
2019-12-21 15:34:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^\w,'-\s]/gi,'')
+  }
+
+  static titleize(string){
+    let splitString = string.split(' ')
+    console.log(splitString)
+    var i;
+    for(i = 0; i < splitString.length; i++){
+      if (splitString[i] === 'a' || 'the' || 'an' || 'but' || 'of' || 'and' || 'for' || 'at' || 'by' || 'from')
+        splitString[i].join(' ')
+        else {
+        splitString[i].capitalize
+        }
+      //return the word then join
+    }
+  }
 }
