f50e558881aaaa73aae1f7f27deffe12f8a09926
Rebecca Tong <rtong@buffalo.edu>
2019-12-23 22:22:37 -0500

    Done.

--- index.js
+++ index.js
@@ -11,16 +11,10 @@ class Formatter {
   static titleize(string){
     let newString = []
     let splitString = string.split(' ')
-
-
     let firstWord = splitString[0].charAt(0).toUpperCase() + splitString[0].slice(1)
 
     newString.push(firstWord)
-    // newArray.push(firstWord.charAt(0).toUpperCase() + firstWord.slice(1))
-
-
     var i;
-    console.log(splitString)
     for(i = 1; i < splitString.length; i++){
        if (splitString[i] === 'a' || splitString[i] === 'the' || splitString[i] === 'an' || splitString[i] === 'but' || splitString[i] === 'of' ||splitString[i] === 'and' || splitString[i] === 'for' || splitString[i] === 'at' || splitString[i] === 'by' || splitString[i] === 'from')
        {
@@ -31,7 +25,7 @@ class Formatter {
         }
       //return the word then join
 
-        // return newString.join(' ')
+
     }
     return newString.join(' ')
   }
