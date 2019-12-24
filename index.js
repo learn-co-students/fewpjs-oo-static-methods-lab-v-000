f3ac078faa87178987a1265e1c5895021712683d
Rebecca Tong <rtong@buffalo.edu>
2019-12-23 22:21:54 -0500

    Done.

--- index.js
+++ index.js
@@ -9,26 +9,31 @@ class Formatter {
   }
 
   static titleize(string){
-    let newArray = []
+    let newString = []
     let splitString = string.split(' ')
 
-    let firstWord = splitString[0]
-    newArray.push(firstWord.charAt(0).toUpperCase() + firstWord.slice(1))
+
+    let firstWord = splitString[0].charAt(0).toUpperCase() + splitString[0].slice(1)
+
+    newString.push(firstWord)
+    // newArray.push(firstWord.charAt(0).toUpperCase() + firstWord.slice(1))
 
 
     var i;
+    console.log(splitString)
     for(i = 1; i < splitString.length; i++){
-       if (splitString[i] === 'a' || 'the' || 'an' || 'but' || 'of' || 'and' || 'for' || 'at' || 'by' || 'from')
+       if (splitString[i] === 'a' || splitString[i] === 'the' || splitString[i] === 'an' || splitString[i] === 'but' || splitString[i] === 'of' ||splitString[i] === 'and' || splitString[i] === 'for' || splitString[i] === 'at' || splitString[i] === 'by' || splitString[i] === 'from')
        {
-        newArray.push(splitString[i])
+        newString.push(splitString[i])
       }
         else {
-        newArray.push(splitString[i].toUpperCase())
+        newString.push(splitString[i].charAt(0).toUpperCase() + splitString[i].slice(1))
         }
       //return the word then join
-        let takeAwayComma = newArray.slice(' ')
-      return takeAwayComma.toString()
+
+        // return newString.join(' ')
     }
+    return newString.join(' ')
   }
 
 }
