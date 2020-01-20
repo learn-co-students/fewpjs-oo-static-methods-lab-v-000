7d8cf388e3b7af076f68815f389855f33268dd17
Leslie Stevens <flymaven@protonmail.com>
2020-01-20 15:09:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    let lowerCaseWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let stringArray = string.split(' ')
+    let newArray = []
+    let firstWord = this.capitalize(stringArray[0])
+    newArray.push(firstWord)
+    for (let i = 1; i < stringArray.length; i++) {
+      if (!lowerCaseWords.includes(stringArray[i])) {
+        newArray.push(this.capitalize(stringArray[i]))
+      } else {
+        newArray.push(stringArray[i])
+      }
+    }
+    return newArray.join(' ')
+  }
 }
