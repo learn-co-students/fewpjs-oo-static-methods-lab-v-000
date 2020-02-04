e2c05da00cb6538c5d28212be9d7b81594948824
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-02-03 16:38:23 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(saying) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = [];
+    let wordzArray = saying.split(" ")
+    for (let n = 0; n < wordzArray.length; n++) {
+      if (n == 0 ) {
+        result.push(this.capitalize(wordzArray[n]))
+      } else {
+        if (exceptions.includes(wordzArray[n])) {
+          result.push(wordzArray[n] )
+        } else {
+          result.push(this.capitalize(wordzArray[n]))
+        }
+      }
+
+    }
+    return result.join(" ");
+  }
 }
