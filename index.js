a11b8c761e3c93313d2f124f1ff3d89e302c2f2b
GiancarloZ <gzabaleta48@gmail.com>
2020-02-11 15:14:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+ 
+  static titleize(string){
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return string.split(" ").map((word, i)=> exceptions.includes(word) && i !== 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+  }
 }
