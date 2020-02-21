8b9847208a8eb518cf422159e6a4fe76820f9a4f
Sushi Chanrai <sushi@pathworks.co>
2020-02-21 15:04:12 -0500

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ class Formatter {
 
   static titleize(sentance){
     let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ];
+    //you have to put result in new array so you can run join on it
     let result = []
     let arrayWords = sentance.split(' ');
-    arrayWords.forEach(word => {
+    arrayWords.map(word => {  // you can use forEach here too
       if (word === arrayWords[0]){
         result.push(this.capitalize(word))
       } else if (exceptions.includes(word)){
