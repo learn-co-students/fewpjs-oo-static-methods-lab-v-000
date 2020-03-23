ef666a8afc3d257b17522591378fc90cf80ba71f
Meg Gutshall <meghan.gutshall@gmail.com>
2020-03-23 19:55:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string) {
+    let words = string.split(" ");
+    let title = words.map(word =>
+      word.replace(/\b(?!the|a|an|but|of|and|for|at|by|from)\b\S+/g, w => w.charAt(0).toUpperCase() + w.slice(1)));
+    title[0].charAt(0).toUpperCase() + title[0].slice(1);
+    return title.join(" ");
+  }
 }
+
