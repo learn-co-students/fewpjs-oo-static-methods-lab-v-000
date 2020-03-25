253c15098b565dd665259eafe4e83fd4e9a02fbd
Meg Gutshall <meghan.gutshall@gmail.com>
2020-03-24 22:41:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 class Formatter {
   static capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+    let phrase = string;
+    return phrase.charAt(0).toUpperCase() + phrase.slice(1);
   }
 
   static sanitize(string) {
-    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+    let phrase = string;
+    return phrase.replace(/[^A-Za-z0-9-'\s]+/g, '');
   }
 
+  // static titleize(string) {
+  //   let phrase = string;
+  //   let array = phrase.split(" ");
+  //   let title = [];
+  //   for (const word of array) {
+  //     let titleWord = word.replace(/\b(?!the|a|an|but|of|and|for|at|by|from)\b\S+/g, (word.charAt(0).toUpperCase() + word.slice(1)));
+  //     title.push(titleWord);
+  //   }
+  //   return Formatter.capitalize(title.join(" "));
+  // }
+
+  /* Above is passing all but one test!!! */
+
   static titleize(string) {
+    let phrase = string;
     let words = string.split(" ");
-    let title = words.map(word =>
-      word.replace(/\b(?!the|a|an|but|of|and|for|at|by|from)\b\S+/g, w => w.charAt(0).toUpperCase() + w.slice(1)));
-    title[0].charAt(0).toUpperCase() + title[0].slice(1);
+    let title = [];
+    let articles = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0) {
+        title.push(this.capitalize(words[i]));
+      } else if (articles.includes(words[i])) {
+        title.push(words[i])
+      } else {
+        title.push(this.capitalize(words[i]))
+      }
+    }
     return title.join(" ");
   }
 }
-
