abd9d78e0ae5e4dea08248fcd8d6772d5b33d7dc
ElvinGarcia <dimix181@gmail.com>
2020-04-25 00:27:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string){
+    return string.replace(/\b[a-zA-Z]/gi, x => x.toUpperCase() )
+
+  }
+
+  static sanitize(string){
+    return string.replace(/[^a-z\s\'\"\-]/gi,"")
+  }
+
+  static titleize(string){
+    const exceptionalWords = ['the','a','an','but','of','and','for','at','by','from']
+    let newstring = string.charAt(0).toUpperCase() + string.slice(1);
+    return newstring.replace(/\b(\w*)/gim, function(x){ 
+      return (exceptionalWords.includes(x)) ? x : (x.charAt(0).toUpperCase() + x.slice(1)) 
+    });
+  }
+
 }
