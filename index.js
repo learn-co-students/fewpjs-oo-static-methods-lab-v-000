bc64a7702a81fee987d8f78cbeda5787a967ad8e
iamtamtamiam <vutam_a@hotmail.com>
2020-05-15 16:19:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(sentence){
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1)
+    //return sentence.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); })
+  }
+
+  static sanitize(sentence){
+    return sentence.replace(/[^A-Za-z0-9-' ]+/g, '')
+
+  }
+
+  static titleize(sentence){
+    let sentence_array = sentence.split(' ')
+    //dont capitalize preps
+
+    let notWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    sentence_array.unshift(Formatter.capitalize(sentence_array.shift()))
+
+
+    let newSentenceArray = sentence_array.map(function(word){
+      if (notWords.includes(word))
+        {return word}
+      else
+        {return Formatter.capitalize(word);}
+      }
+    )
+    
+    let newSentence = newSentenceArray.join(" ")
+    return newSentence
+
+  }
+
 }
