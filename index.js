671b0953c42091b71ccc5a837fd14eab3cec4645
sjwakeman <swakeman@ca.rr.com>
2020-07-03 09:14:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
   //add static methods here
+  // is a static method
+  static capitalize(sentence) {
+    // console.log("sentence", sentence)
+    // sentence a
+    // uppercases the first letter in a String
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1)
+  }
+  // is a static method
+  static sanitize(sentence) {
+    // console.log("sentence", sentence)
+    // sanitize
+    // sentence C catching colds
+    // removes non-alphanumeric characters except for dash, single quote and space
+    return sentence.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(sentence) {
+    // console.log("sentence", sentence)
+    // sentence F forever foolish
+    // sentence getting giggles
+    let modifiedSentence = sentence.split(" ")
+    // console.log("modifiedSentence", modifiedSentence)
+    // modifiedSentence [ 'F', 'forever', 'foolish' ]
+    // modifiedSentence [ 'getting', 'giggles' ]
+    // modifiedSentence [ 'a', 'tale', 'of', 'two', 'cities' ]
+    console.log("modifiedSentence.charAt(0).toUpperCase() + sentence.slice(1)", modifiedSentence.charAt(0).toUpperCase() + sentence.slice(1))
+
+  } 
 }
