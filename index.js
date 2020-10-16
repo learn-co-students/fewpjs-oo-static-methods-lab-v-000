e84cf173442dddd1a7877d627e2c60ec2128fdaf
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-16 19:59:12 -0400

    Done.

--- index.js
+++ index.js
@@ -16,9 +16,17 @@ class Formatter {
     return string.replace(/[^A-Za-z0-9 '-]/g, "");
   }
 
-  static titleize(string) {
-    // return string.split(' ').map(&:capitalize).join(' ');
-    // [0].toUpperCase() + string.slice(1);
+   static titleize(str) {
+    let badArr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let firstWord = str.split(' ')[0]
+
+    return str.split(' ').map(word => {
+      if (badArr.includes(word) && word !== firstWord) {
+        return word.toLowerCase();
+      } else {
+        return word[0].toUpperCase() + word.slice(1)
+      }
+    }).join(' ')
   }
 
 }
