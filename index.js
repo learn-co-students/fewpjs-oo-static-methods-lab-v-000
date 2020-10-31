6685a4881875874bd54289450785504eee7832eb
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-31 03:49:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    let str = string.charAt(0).toUpperCase() + string.slice(1);
+    return str;
+  }
+
+  static sanitize(string) {
+    const regex = /[^A-Za-z0-9\s-\']+/g
+    return string.replace(regex, '');
+  }
+
+  static findWord(word) {
+    let forbiddenWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    return forbiddenWords.some(element => element === word);
+  }
+
+  static titleize(string) {
+    let array = string.split(" ");
+    let titleizeString = [];
+
+    titleizeString = array.map((word, index) => {
+      if(index > 0 && this.findWord(word)) {
+        return word;
+      }else{
+        return this.capitalize(word);
+      }
+    });
+    return titleizeString.join(" ");
+  }
 }
