6b97dee9ed56b4cc6f0d2ad5da17d3737da6e3f1
wakenightRyu <bennyhudsonhung@gmail.com>
2020-12-24 00:10:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    const forbidden = ['a','an','the','but','of','and','for','at','by','from']
+
+    let newString = []
+
+    string.split(" ").forEach(word => {
+      if (forbidden.includes(word) === false) {
+        word = word.charAt(0).toUpperCase() + word.slice(1)
+      }
+      else {
+        word
+      }
+      newString.push(word)
+    })
+    let joinString = newString.join(' ')
+    return joinString.charAt(0).toUpperCase() + joinString.slice(1)
+  }
 }
