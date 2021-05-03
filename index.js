361d88ea4d849bdec07f47c54d7256841c930ad9
klovae <elyse.klova@gmail.com>
2021-05-03 18:30:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(str) {
+    const checkArray = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const stringArray = str.split(' ');
+    const titleArray = stringArray.map(function(element) {
+      if (!checkArray.find(x => x === element)) {
+        return Formatter.capitalize(element)
+      } else { return element };
+    })
+    const titleized =  titleArray.join(' ');
+    return Formatter.capitalize(titleized)
+    }
 }
